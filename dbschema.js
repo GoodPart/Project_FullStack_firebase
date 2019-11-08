32dab29ccae1679b25c8d27e483c86a9da5d98a9
GoodPart <pks940122@cttd.co.kr>
2019-11-08 14:53:07 +0900

    firebase-function
    한대까지 커밋

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2019-11-08T00:50:42.341Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
